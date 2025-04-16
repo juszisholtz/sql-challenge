7614e123590fe0b2fbd66560522425222e343e23
juszisholtz <juszisholtz@gmail.com>
2025-04-16 17:47:09 -0400

    added the varchar limits

--- EmployeeSQL/SQL Files/Table Schema QuickDBD.sql
+++ EmployeeSQL/SQL Files/Table Schema QuickDBD.sql
@@ -2,58 +2,70 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
+-- Drop tables if they exist, in order to avoid foreign key constraint errors
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS titles;
+
+-- Recreate tables with all VARCHARs set to VARCHAR(30)
+
 CREATE TABLE "titles" (
-    "title_id" VARCHAR   NOT NULL,
-    "title" VARCHAR   NOT NULL,
+    "title_id" VARCHAR(30) NOT NULL,
+    "title" VARCHAR(30) NOT NULL,
     CONSTRAINT "pk_titles" PRIMARY KEY (
         "title_id"
      )
 );
 
 CREATE TABLE "employees" (
-    "emp_no" INT   NOT NULL,
-    "emp_title_id" VARCHAR   NOT NULL,
-    "birth_date" DATE   NOT NULL,
-    "first_name" VARCHAR   NOT NULL,
-    "last_name" VARCHAR   NOT NULL,
-    "sex" VARCHAR   NOT NULL,
-    "hire_date" DATE   NOT NULL,
+    "emp_no" INT NOT NULL,
+    "emp_title_id" VARCHAR(30) NOT NULL,
+    "birth_date" DATE NOT NULL,
+    "first_name" VARCHAR(30) NOT NULL,
+    "last_name" VARCHAR(30) NOT NULL,
+    "sex" VARCHAR(30) NOT NULL,
+    "hire_date" DATE NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY (
         "emp_no"
      )
 );
 
 CREATE TABLE "departments" (
-    "dept_no" VARCHAR   NOT NULL,
-    "dept_name" VARCHAR   NOT NULL,
+    "dept_no" VARCHAR(30) NOT NULL,
+    "dept_name" VARCHAR(30) NOT NULL,
     CONSTRAINT "pk_departments" PRIMARY KEY (
         "dept_no"
      )
 );
 
 CREATE TABLE "dept_emp" (
-    "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR   NOT NULL,
+    "emp_no" INT NOT NULL,
+    "dept_no" VARCHAR(30) NOT NULL,
     CONSTRAINT "pk_dept_emp" PRIMARY KEY (
         "emp_no","dept_no"
      )
 );
 
 CREATE TABLE "dept_manager" (
-    "dept_no" VARCHAR   NOT NULL,
-    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR(30) NOT NULL,
+    "emp_no" INT NOT NULL,
     CONSTRAINT "pk_dept_manager" PRIMARY KEY (
         "dept_no","emp_no"
      )
 );
 
 CREATE TABLE "salaries" (
-    "emp_no" INT   NOT NULL,
-    "salary" INT   NOT NULL,
+    "emp_no" INT NOT NULL,
+    "salary" INT NOT NULL,
     CONSTRAINT "pk_salaries" PRIMARY KEY (
         "emp_no"
      )
 );
+
+-- Foreign keys
 
 ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
 REFERENCES "titles" ("title_id");
